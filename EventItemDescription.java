b8f692fd484820da154d8e5f4716ad6582ed0daf
praneethvallabhaneni <71108379+praneethvallabhaneni@users.noreply.github.com>
2021-10-19 17:11:35 -0500

    added_event_item_description

---
+++ EventItemDescription.java
@@ -0,0 +1,14 @@
+package com.example.never_forget_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventItemDescription extends AppCompatActivity {
+// praneeth has commented
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_item_description);
+    }
+}

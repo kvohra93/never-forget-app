796005854a6ce631ec8d235641b23d8c56338a89
Christopher Burke <merelyChris@users.noreply.github.com>
2021-11-19 23:36:37 -0600

    WIP reminder fragment

---
+++ app/src/main/java/com/example/never_forget_app/NewReminder.java
@@ -0,0 +1,56 @@
+package com.example.never_forget_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewReminder extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NewReminder() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NewReminder.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NewReminder newInstance() {
+        NewReminder fragment = new NewReminder();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_reminder, container, false);
+    }
+
+    private ReminderModel myReminderModel;
+
+
+}
